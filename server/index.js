4221c7af4c73b40481de7660157f62d92aeb9043
Daniel Kim <dannykim200@gmail.com>
2021-03-21 21:08:12 -0400

    setting up node server for spotify authentication

--- server/index.js
+++ server/index.js
@@ -1,8 +1,31 @@
-const express = require("express");
+const express = require('express');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const SpotifyStrategy = require('passport-spotify').Strategy;
+const bodyParser = require('body-parser');
+const production = process.env.NODE_ENV == 'production';
+
+// setting up the Spotify API
+const SpotifyWebApi = require('spotify-web-api-node');
+const spotifyApi = new SpotifyWebApi({
+  clientId: '',
+  clientSecret: '',
+  scope: ''
+});
+
+// Spotify Login
+const app = express();
+app.use(cookieParser());
+
+if (production) {
+  app.use(express.static('./dist'));
+} else {
+  app.use(express.static('./client/public'));
+}
+
+// Set up Passport
 
 const PORT = process.env.PORT || 3001;
-
-const app = express();
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
