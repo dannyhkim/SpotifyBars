dff53edacf73e696ceffc7003172dbbe0b204899
Daniel Kim <dannykim200@gmail.com>
2021-03-29 21:03:42 -0400

    grab song data from res

--- client/src/store/actions/song.js
+++ client/src/store/actions/song.js
@@ -6,16 +6,40 @@ export const getCurrentSong = () => {
     // first dispatch immediate synchronous action to store to indicate you've started fetching the current song data
     dispatch(getCurrentSongStarted());
 
-    // get request for current song
-    axios
-      .get("http://localhost:4000/api/getSong", { withCredentials: true })
-      .then((res) => {
-        console.log("Response: " + JSON.stringify(res.data));
-        dispatch(getCurrentSongSuccess(res.data));
-      })
-      .catch((err) => {
-        dispatch(getCurrentSongFailure(err.message));
-      });
+    return new Promise((resolve, reject) => {
+      // get request for current song
+      axios
+        .get("/api/getSong", { withCredentials: true })
+        .then((res) => {
+          const data = res.data;
+
+          if (data.err) {
+            return reject(data.err);
+          }
+
+          const result = data.result.body;
+          let songData = null;
+
+          if (result.is_playing) {
+            songData = {
+              album: result.item.album.name,
+              artist: result.item.artists[0].name,
+              trackName: result.item.name,
+              progress: result.progress_ms,
+              duration: result.item.duration_ms,
+              isPlaying: true,
+            };
+          }
+          console.log("Response: " + JSON.stringify(res.data));
+          dispatch(getCurrentSongSuccess(songData));
+          return resolve();
+        })
+        .catch((err) => {
+          console.log("getCurrentSong failed: " + err.message);
+          dispatch(getCurrentSongFailure({ song: { isPlaying: false } }));
+          return resolve();
+        });
+    });
   };
 };
 
